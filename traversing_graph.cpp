9111e00dd10dea4707bfa2621f1f7b77551a724a
Ritik Bosu <bosuritik@gmail.com>
2024-03-14 11:17:38 +0530

    new

--- traversing_graph.cpp
+++ traversing_graph.cpp
@@ -1,23 +1,78 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
 
-using namespace std;
+class Graph {
+public:
+    Graph(int numVertices) : adjacencyList(numVertices) {}
 
-int main()
-{
-    int mat[4][4]={
-        {1,2,3,4},
-        {5,6,7,8},
-        {9,10,11,12},
-        {13,14,15,16},
-    };
-    int r=sizeof(mat)/sizeof(mat[0]);
-    int c=sizeof(mat)/sizeof(mat[0]);
-    vecor<vector<int> > visited(r, vector<int> c,0)
-    for(int i=0; i<r; i++){
-        for(int j=0; j<c; j++){
-            if(!visited[i][j]){}
-            cout<<i<<","<<j<<endl;
+    void addEdge(int u, int v) {
+        adjacencyList[u].push_back(v);
+        adjacencyList[v].push_back(u);
+    }
+
+    void print() {
+        for (int i = 0; i < adjacencyList.size(); ++i) {
+            std::cout << i << ": ";
+            for (int v : adjacencyList[i]) {
+                std::cout << v << " ";
+            }
+            std::cout << std::endl;
         }
     }
+
+    void bfs(int start) {
+        std::vector<bool> visited(adjacencyList.size(), false);
+        std::vector<int> queue;
+
+        queue.push_back(start);
+        visited[start] = true;
+
+        while (!queue.empty()) {
+            int current = queue.front();
+            queue.erase(queue.begin());
+
+            std::cout << current << " ";
+
+            for (int v : adjacencyList[current]) {
+                if (!visited[v]) {
+                    queue.push_back(v);
+                    visited[v] = true;
+                }
+            }
+        }
+    }
+
+private:
+    std::vector<std::vector<int>> adjacencyList;
+};
+
+int main() {
+    int numVertices, numEdges, u, v;
+
+    std::cout << "Enter the number of vertices: ";
+    std::cin >> numVertices;
+
+    Graph graph(numVertices);
+
+    std::cout << "Enter the number of edges: ";
+    std::cin >> numEdges;
+
+    std::cout << "Enter the edges (u v):" << std::endl;
+    for (int i = 0; i < numEdges; ++i) {
+        std::cin >> u >> v;
+        graph.addEdge(u, v);
+    }
+
+    std::cout << "The graph is: " << std::endl;
+    graph.print();
+
+    int start;
+    std::cout << "\nEnter the starting vertex for BFS traversal: ";
+    std::cin >> start;
+
+    std::cout << "\nBFS traversal starting from vertex " << start << ": ";
+    graph.bfs(start);
+
+    return 0;
 }
